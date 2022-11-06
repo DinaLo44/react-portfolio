885dc408316efd77670ce7c4f6e0732ec9c3f2d2
DinaLo <dinalo.brady@gmail.com>
2022-11-06 13:46:13 -0700

    created app.s js and imported the nav pages. Added images as well

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import "./index.css";
+import Home from "./nav-pages/Home";
+import About from "./nav-pages/About";
+import Portfolio from "./nav-pages/Project";
+import Contact from "./nav-pages/Contact";
+import Resume from "./nav-pages/Resume";
 
-import Navigation from './components/Navigation';
+import { Route, Routes } from 'react-router-dom';
 
 
 function App() {
-  return <Navigation />;
+  return (
+<>
+<Routes>
+  <Route path= "/" element ={<Home/>} />
+  <Route path= "/about" element ={<About/>} />
+  <Route path= "/portfolio" element ={<Portfolio />} />
+  <Route path= "/contact" element ={<Contact/>} />
+  <Route path= "/resume" element ={<Resume/>} />
+</Routes>
+</>
+  ) 
 }
 
 export default App;
---
+++ react-portfolio/src/nav-pages/Project.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Project = () => {
+  return (
+    <div>Portfolio</div>
+  )
+}
+
+export default Project
