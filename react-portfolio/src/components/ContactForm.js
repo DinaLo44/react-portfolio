09452ac0ec7616d4539b95062c90fb17ab41c938
DinaLo <dinalo.brady@gmail.com>
2022-11-10 14:38:07 -0700

    worked of contact form styling and event handler

--- react-portfolio/src/components/ContactForm.js
+++ react-portfolio/src/components/ContactForm.js
@@ -1,17 +1,38 @@
 import React from 'react'
 import "./ContactFormStyles.css";
+// import "./HandleContactForm";
+import { useState } from 'react';
 
 const ContactForm = () => {
-  return (
+  const [contactName, setContactName] = useState('');
+  const [contactEmail, setContactEmail]= useState('');
+
+  const handleInputField = (e) =>{
+      const {name, value} = e.target;
+      return name === 'contactName' ? setContactName(value) : setContactEmail(value);
+  }
+
+  const handleContactFormSubmission = (e) => {
+      e.preventDefault();
+      if (contactName === '') {
+          alert ("Name is a required field");
+      } else if (contactEmail === '') {
+          alert ("An email address is a required field");
+      }
+      setContactName('');
+      setContactEmail('');
+  };return (
     <div className="contact-form">
         <form>
-            <label>Name</label>
-            <input type="text"></input>
-            <label>E-mail</label>
-            <input type="email"></input>
+            <label value={contactName} name="contactName" onChange={handleInputField}>Name</label>
+            <input type="text" placeholder="Type your name"></input>
+            <label value={contactEmail} name="contactEmail" onChange={handleInputField} >E-mail</label>
+            <input type="email" placeholder="Type your email"></input>
             <label>Message</label>
-            <textarea rows="10" placeholder="Please type your message inside this box"></textarea>
-        <button className="submit-button">Submit</button>
+            <textarea rows="10" placeholder="Type your message inside this box"></textarea>
+        {/* <a href="mailto:dinalo.brady@gmail.com"> */}
+        <button onClick={handleContactFormSubmission} className="submit-button">Submit</button>
+        {/* </a> */}
         </form>
     </div>
   )
