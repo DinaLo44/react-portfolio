5db0acb1e00d48a3825a7a6d1c9b5f5d2ee61e01
DinaLo <dinalo.brady@gmail.com>
2022-11-14 18:16:52 -0700

    hashrouter

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from "react";
 // import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 
 const root= createRoot(document.getElementById("root"))
 
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
 
 );
 
